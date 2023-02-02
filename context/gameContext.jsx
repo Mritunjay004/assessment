4f88b75b086b85bcc1122030316d873b9d0f47e3
Mritunjay Gupta <mritunjaygupta004@gmail.com>
2023-02-03 00:29:02 +0530

    cleaning stuff

--- context/gameContext.jsx
+++ context/gameContext.jsx
@@ -1,10 +1,52 @@
-const { createContext } = require("react");
+const { createContext, useEffect, useState } = require("react");
 
 const GameContext = createContext();
 
 export default function GameProvider({ children }) {
+  const defaultGames = [
+    {
+      id: 1,
+      name: "Bottle Flip",
+      url: "https://simpleiralgames.com",
+      author: "Simple Viral Games",
+      published_date: "2022-08-01 00:00:00",
+    },
+    {
+      id: 2,
+      name: "Tic Tac Toe",
+      url: "https://minimalisticgames.com",
+      author: "Minimalistic Games",
+      published_date: "2022-06-15 00:00:00",
+    },
+    {
+      id: 3,
+      name: "Rock, Paper, Scissors",
+      url: "https://classicsandbeyond.com",
+      author: "Classics and Beyond",
+      published_date: "2022-09-21 00:00:00",
+    },
+    {
+      id: 4,
+      name: "Solitaire",
+      url: "https://cardgamesinc.com",
+      author: "Card Games Inc.",
+      published_date: "2022-07-02 00:00:00",
+    },
+  ];
+
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.games) {
+      setGames(JSON.parse(localStorage.games));
+    } else {
+      setGames(defaultGames);
+      localStorage.setItem("games", JSON.stringify(defaultGames));
+    }
+  }, []);
+
   async function getAllGames() {
-    return localStorage.games;
+    return localStorage.getItem("games");
   }
 
   async function updateGame(game) {
@@ -16,11 +58,39 @@ export default function GameProvider({ children }) {
       }
       return g;
     });
+    setGames(updatedGames);
     localStorage.setItem("games", JSON.stringify(updatedGames));
   }
 
+  async function addGame(e) {
+    e.preventDefault();
+    const newGame = {
+      name: e.target.name.value,
+      url: e.target.url.value,
+      author: e.target.author.value,
+      published_date: e.target.published_date.value,
+
+      //   sets to a unique number
+      id: Math.floor(Math.random() * 100000000),
+    };
+    setGames([...games, newGame]);
+    localStorage.setItem("games", JSON.stringify([...games, newGame]));
+    window.location.reload();
+  }
+
+  async function deleteGame(id) {
+    const data = await getAllGames();
+    const games = JSON.parse(data);
+    const updatedGames = games.filter((game) => game.id !== id);
+    setGames(updatedGames);
+    localStorage.setItem("games", JSON.stringify(updatedGames));
+    window.location.reload();
+  }
+
   return (
-    <GameContext.Provider value={{ getAllGames, updateGame }}>
+    <GameContext.Provider
+      value={{ getAllGames, updateGame, addGame, deleteGame }}
+    >
       {children}
     </GameContext.Provider>
   );
--- src/pages/[id].jsx
+++ src/pages/[id].jsx
@@ -4,18 +4,16 @@ import React, { useContext, useEffect, useState } from "react";
 
 const GameInfo = ({ id }) => {
   const { getAllGames, updateGame } = useContext(GameContext);
-  const games = getAllGames();
-  //   console.log(games);
 
-  const [data, setData] = useState();
-  const retrieveDataFromPromise = async () => {
-    const promiseData = await games;
-    console.log(JSON.parse(promiseData));
-    setData(JSON.parse(promiseData));
-  };
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    retrieveDataFromPromise();
+    async function fetchData() {
+      const response = await getAllGames();
+      const json = await response;
+      setData(JSON.parse(json));
+    }
+    fetchData();
   }, []);
 
   // edit a game
@@ -28,7 +26,6 @@ const GameInfo = ({ id }) => {
     gameToEdit.published_date = e.target.published_date.value;
     updateGame(gameToEdit);
 
-    // reload the page to show the updated data
     window.location.reload();
   };
 
@@ -36,101 +33,109 @@ const GameInfo = ({ id }) => {
 
   return (
     <div>
-      {data &&
-        data.map((game) => {
-          if (game.id == id) {
-            return (
-              <div className="grid justify-center" key={game.id}>
-                <div>
-                  <h1 className="text-center mt-20 text-5xl ">{game.name}</h1>
-                  <p className="mt-3">URL: {game.url}</p>
-                  <p className="mt-3">Author: {game.author}</p>
-                  <p className="mt-3">
-                    Published On: <hr />
-                    {new Date(game.published_date).toLocaleDateString("en-US", {
-                      weekday: "long",
-                      year: "numeric",
-                      month: "long",
-                      day: "numeric",
-                    })}
-                  </p>
-                </div>
-
-                <div className="flex gap-10 mt-10 ">
-                  {/* Edit a game */}
+      {data === null ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {data.map((game) => {
+            if (game.id == id) {
+              return (
+                <div className="grid justify-center" key={game.id}>
                   <div>
-                    <button
-                      className="bg-blue-500 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                      onClick={() => {
-                        setShowEditForm(!showEditForm);
-                      }}
-                    >
-                      Edit
-                    </button>
+                    <h1 className="text-center mt-20 text-5xl ">{game.name}</h1>
+                    <p className="mt-3">URL: {game.url}</p>
+                    <p className="mt-3">Author: {game.author}</p>
+                    <p className="mt-3">
+                      Published On: <hr />
+                      {new Date(game.published_date).toLocaleDateString(
+                        "en-US",
+                        {
+                          weekday: "long",
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        }
+                      )}
+                    </p>
+                  </div>
 
-                    {/* should be hidden by default and only show when the button is clicked */}
-                    {showEditForm && (
-                      <form
-                        className=" flex flex-col items-center justify-center w-full h-full max-w-2xl p-4 mx-auto bg-white rounded-lg shadow-xl"
-                        onSubmit={(e) => {
-                          editGame(e, game.id);
-                          setShowEditForm(false);
+                  <div className="flex gap-10 mt-10 ">
+                    {/* Edit a game */}
+                    <div>
+                      <button
+                        className="bg-blue-500 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => {
+                          setShowEditForm(!showEditForm);
                         }}
                       >
-                        <label htmlFor="name">Name</label>
-                        <input
-                          className="border border-solid "
-                          type="text"
-                          id="name"
-                          name="name"
-                        />
-                        <label htmlFor="url">URL</label>
-                        <input
-                          className="border border-solid "
-                          type="text"
-                          id="url"
-                          name="url"
-                        />
-
-                        <label htmlFor="author">Author</label>
-                        <input
-                          className="border border-solid "
-                          type="text"
-                          id="author"
-                          name="author"
-                        />
-
-                        <label htmlFor="published_date">Published Date</label>
-                        <input
-                          className="border border-solid "
-                          type="date"
-                          id="published_date"
-                          name="published_date"
-                        />
-
-                        <button
-                          className="bg-green-500 mt-2 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                          type="submit"
-                        >
-                          Submit
-                        </button>
-                      </form>
-                    )}
-                  </div>
-
-                  <div>
-                    {/* home button */}
-                    <Link href="/home">
-                      <button className="bg-blue-500 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Home
+                        Edit
                       </button>
-                    </Link>
+
+                      {/* should be hidden by default and only show when the button is clicked */}
+                      {showEditForm && (
+                        <form
+                          className=" flex flex-col items-center justify-center w-full h-full max-w-2xl p-4 mx-auto bg-white rounded-lg shadow-xl"
+                          onSubmit={(e) => {
+                            editGame(e, game.id);
+                            setShowEditForm(false);
+                          }}
+                        >
+                          <label htmlFor="name">Name</label>
+                          <input
+                            className="border border-solid "
+                            type="text"
+                            id="name"
+                            name="name"
+                          />
+                          <label htmlFor="url">URL</label>
+                          <input
+                            className="border border-solid "
+                            type="text"
+                            id="url"
+                            name="url"
+                          />
+
+                          <label htmlFor="author">Author</label>
+                          <input
+                            className="border border-solid "
+                            type="text"
+                            id="author"
+                            name="author"
+                          />
+
+                          <label htmlFor="published_date">Published Date</label>
+                          <input
+                            className="border border-solid "
+                            type="date"
+                            id="published_date"
+                            name="published_date"
+                          />
+
+                          <button
+                            className="bg-green-500 mt-2 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                            type="submit"
+                          >
+                            Submit
+                          </button>
+                        </form>
+                      )}
+                    </div>
+
+                    <div>
+                      {/* home button */}
+                      <Link href="/home">
+                        <button className="bg-blue-500 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                          Home
+                        </button>
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          }
-        })}
+              );
+            }
+          })}
+        </div>
+      )}
     </div>
   );
 };
